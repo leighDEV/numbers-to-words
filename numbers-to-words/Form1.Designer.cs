b8c09283aa3d86d5a2d4827366268ff9e69c86e4
leighDEV <leighest2021@gmail.com>
2022-03-22 13:35:25 +0800

    fix: update code
    still not working

--- numbers-to-words/Form1.Designer.cs
+++ numbers-to-words/Form1.Designer.cs
@@ -28,34 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.tbxNumbers = new System.Windows.Forms.TextBox();
+            this.lblText = new System.Windows.Forms.Label();
+            this.lblNumbers = new System.Windows.Forms.Label();
+            this.lblWords = new System.Windows.Forms.Label();
+            this.ptbText = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbText)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // btnConvert
             // 
-            this.button1.Location = new System.Drawing.Point(280, 215);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(260, 66);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnConvert.Location = new System.Drawing.Point(39, 108);
+            this.btnConvert.Margin = new System.Windows.Forms.Padding(2);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(126, 33);
+            this.btnConvert.TabIndex = 0;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(169, 108);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(2);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(126, 33);
+            this.btnClear.TabIndex = 0;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // tbxNumbers
+            // 
+            this.tbxNumbers.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxNumbers.Location = new System.Drawing.Point(39, 72);
+            this.tbxNumbers.Name = "tbxNumbers";
+            this.tbxNumbers.Size = new System.Drawing.Size(490, 31);
+            this.tbxNumbers.TabIndex = 1;
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.BackColor = System.Drawing.Color.White;
+            this.lblText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblText.Location = new System.Drawing.Point(48, 199);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(60, 24);
+            this.lblText.TabIndex = 2;
+            this.lblText.Text = "label1";
+            // 
+            // lblNumbers
+            // 
+            this.lblNumbers.AutoSize = true;
+            this.lblNumbers.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumbers.Location = new System.Drawing.Point(34, 44);
+            this.lblNumbers.Name = "lblNumbers";
+            this.lblNumbers.Size = new System.Drawing.Size(98, 25);
+            this.lblNumbers.TabIndex = 3;
+            this.lblNumbers.Text = "Numbers";
+            // 
+            // lblWords
+            // 
+            this.lblWords.AutoSize = true;
+            this.lblWords.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWords.Location = new System.Drawing.Point(34, 158);
+            this.lblWords.Name = "lblWords";
+            this.lblWords.Size = new System.Drawing.Size(74, 25);
+            this.lblWords.TabIndex = 3;
+            this.lblWords.Text = "Words";
+            // 
+            // ptbText
+            // 
+            this.ptbText.BackColor = System.Drawing.Color.White;
+            this.ptbText.Location = new System.Drawing.Point(39, 186);
+            this.ptbText.Name = "ptbText";
+            this.ptbText.Size = new System.Drawing.Size(490, 102);
+            this.ptbText.TabIndex = 4;
+            this.ptbText.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(584, 366);
+            this.Controls.Add(this.lblWords);
+            this.Controls.Add(this.lblNumbers);
+            this.Controls.Add(this.lblText);
+            this.Controls.Add(this.tbxNumbers);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.ptbText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Numbers to Words Converter";
+            ((System.ComponentModel.ISupportInitialize)(this.ptbText)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox tbxNumbers;
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.Label lblNumbers;
+        private System.Windows.Forms.Label lblWords;
+        private System.Windows.Forms.PictureBox ptbText;
     }
 }
 
--- numbers-to-words/Form1.cs
+++ numbers-to-words/Form1.cs
@@ -17,9 +17,70 @@ namespace numbers_to_words
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btnConvert_Click(object sender, EventArgs e)
         {
+            try
+            {
+                int i = 1;
 
+                string words = tbxNumbers.Text;
+                int num = Convert.ToInt32(words);
+                while (i > 0)
+                {
+                    if (num.Equals(0))
+                    {
+                        lblText.Text = "zero";
+                    }
+                    if (num > 9999)
+                    {
+                        lblText.Text = "Error. Number should be less than 10000.\n";
+                        continue;
+                    }
+
+                    Converting(num);
+                    i++;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            tbxNumbers.Clear();
+        }
+
+        private void Converting(int num)
+        {
+            string[] words = new string[] { "", "one", "two", "three", "four", "five", "six", "seven",
+        "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+        "eighteen", "nineteen", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+
+            if (num > 999)
+            {
+                int n = num / 1000;
+                lblText.Text = $"{words[n]} thousand ";
+                num -= n * 1000;
+            }
+
+            if (num > 99)
+            {
+                int n = num / 100;
+                lblText.Text = $"{words[n]} hundred ";
+                num -= n * 100;
+            }
+
+            if (num > 20)
+            {
+                int n = num / 10;
+                lblText.Text = $"{words[n + 18]} ";
+                num -= n * 10;
+            }
+            lblText.Text = words[num];
         }
     }
 }
