ad69d7c604cfd9be91e9e6d96e418de29592cc88
leighDEV <leighest2021@gmail.com>
2022-04-09 14:07:13 +0800

    syle: add icon

--- numbers-to-words/Form1.Designer.cs
+++ numbers-to-words/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.lblNumbers = new System.Windows.Forms.Label();
             this.lblWords = new System.Windows.Forms.Label();
             this.tbNumbers = new System.Windows.Forms.TextBox();
@@ -115,6 +116,7 @@
             this.Controls.Add(this.lblNumbers);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.Name = "Form1";
